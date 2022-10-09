30fb755ea740fb481336817c8b4cb427c37a97b9
Santiago Pérez <santipego0001@gmail.com>
2022-10-09 11:31:32 -0500

    Added file

--- src/main/java/mintic/proyectotutoria/ProyectotutoriaApplication.java
+++ src/main/java/mintic/proyectotutoria/ProyectotutoriaApplication.java
@@ -1,15 +1,10 @@
 package mintic.proyectotutoria;
 
-import mintic.proyectotutoria.modelo.Gato;
 import mintic.proyectotutoria.repositorio.RepositorioGato;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
-import java.util.List;
 
 @SpringBootApplication
 public class ProyectotutoriaApplication {
@@ -17,12 +12,5 @@ public class ProyectotutoriaApplication {
 	private RepositorioGato repo;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectotutoriaApplication.class, args);
-	}
-	@Bean
-	ApplicationRunner applicationRunner(){
-		return args -> {
-			List<Gato> gatos = repo.obtenerTodos();
-			System.out.println("Gatos: "+gatos.size());
-		};
 	}
 }
