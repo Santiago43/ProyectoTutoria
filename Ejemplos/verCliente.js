2a85a8298b71b45083e2060dc6b9ad78a6e970ff
Santiago Pérez <santipego0001@gmail.com>
2022-10-09 11:34:28 -0500

    Revert "Added code example"
    This reverts commit a74bdaec9a154eaf30778a02c39c2bc2ebac660c.

--- Ejemplos/verCliente.js
+++
@@ -1,7 +0,0 @@
-let queryStrings = new URLSearchParams(window.location.search)
-let parametrosGet = Object.fromEntries(queryStrings.entries())
-
-$(document).ready(function(){
-    //GET ajax cliente/:id
-    console.log("Se ve el cliente con el id ", parseInt(parametrosGet.id))
-})
